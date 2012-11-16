679ce130e88766b7a14a9b75a069494994b57938
beepscore <support@beepscore.com>
2012-11-16 08:12:58 -0800

    In Xcode add group Models and file Location.

---
+++ WhatsMySpeed/Location.h
@@ -0,0 +1,13 @@
+//
+//  Location.h
+//  WhatsMySpeed
+//
+//  Created by Steve Baker on 11/16/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Location : NSObject
+
+@end
---
+++ WhatsMySpeed/Location.m
@@ -0,0 +1,13 @@
+//
+//  Location.m
+//  WhatsMySpeed
+//
+//  Created by Steve Baker on 11/16/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import "Location.h"
+
+@implementation Location
+
+@end
