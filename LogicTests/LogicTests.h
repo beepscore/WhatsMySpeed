5059f4f453ce015da8f8f0cf102500882626b16b
beepscore <support@beepscore.com>
2012-11-16 00:20:32 -0800

    In Xcode project add new Target use template CocoaTouch unit testing bundle. Name target LogicTests. Manually adding a unit test target adds "logic tests" not "application tests".

---
+++ LogicTests/LogicTests.h
@@ -0,0 +1,13 @@
+//
+//  LogicTests.h
+//  LogicTests
+//
+//  Created by Steve Baker on 11/16/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LogicTests : SenTestCase
+
+@end
---
+++ LogicTests/LogicTests.m
@@ -0,0 +1,32 @@
+//
+//  LogicTests.m
+//  LogicTests
+//
+//  Created by Steve Baker on 11/16/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import "LogicTests.h"
+
+@implementation LogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LogicTests");
+}
+
+@end
