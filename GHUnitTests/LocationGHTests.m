bcb937a7c4dba1ed862b95e8a581de763aabb7ec
beepscore <support@beepscore.com>
2012-11-18 11:27:20 -0800

    Add LocationGHTests.m
    Copy first example from GHUnit web site
    http://gabriel.github.com/gh-unit/docs/appledoc_include/guide_testing.html
    
    GHAssertNotNULL doesn't work with ARC, change it to GHAssertNotNil
    Change testFoo to make it pass.

---
+++ GHUnitTests/LocationGHTests.m
@@ -0,0 +1,56 @@
+//
+//  LocationGHTests.m
+//  WhatsMySpeed
+//
+//  Created by Steve Baker on 11/18/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+
+@interface LocationGHTests : GHTestCase { }
+@end
+
+@implementation LocationGHTests
+
+- (BOOL)shouldRunOnMainThread {
+    // By default NO, but if you have a UI test or test dependent on running on the main thread return YES.
+    // Also an async test that calls back on the main thread, you'll probably want to return YES.
+    return NO;
+}
+
+- (void)setUpClass {
+    // Run at start of all tests in the class
+}
+
+- (void)tearDownClass {
+    // Run at end of all tests in the class
+}
+
+- (void)setUp {
+    // Run before each test method
+}
+
+- (void)tearDown {
+    // Run after each test method
+}
+
+- (void)testFoo {
+    // NSString *a = @"foo";
+    NSString *a = @"a string";
+    GHTestLog(@"I can log to the GHUnit test console: %@", a);
+    
+    // Assert a is not nil, with no custom error description
+    GHAssertNotNil(a, nil);
+    
+    // Assert equal objects, add custom error description
+    // NSString *b = @"bar";
+    NSString *b = @"a string";
+    GHAssertEqualObjects(a, b, @"A custom error message. a should be equal to: %@.", b);
+}
+
+- (void)testBar {
+    // Another test
+}
+
+@end
