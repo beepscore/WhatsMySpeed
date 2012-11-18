157f6bb38bfcfe68b735f26b4d7bf73656fc1ea3
beepscore <support@beepscore.com>
2012-11-18 09:24:30 -0800

    In Xcode project add Target empty application GHUnitTests, DON'T select "Include Unit Tests".
    Reference:
    http://gabriel.github.com/gh-unit/docs/appledoc_include/guide_install_ios_4.html

---
+++ GHUnitTests/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GHUnitTests
+//
+//  Created by Steve Baker on 11/18/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
