896d9ee0c44cab70de2893c28058b101deb18ad0
beepscore <support@beepscore.com>
2012-11-18 11:58:18 -0800

    Add WeatherAPITests.
    Reference:
    ExampleAsyncTest
    http://gabriel.github.com/gh-unit/docs/appledoc_include/guide_testing.html
    
    Delete typo in sample duplicate http://www.google.com
    Delete non ARC compliant code - release and autorelease

---
+++ GHUnitTests/WeatherAPITests.m
@@ -0,0 +1,58 @@
+//
+//  WeatherAPITests.m
+//  WhatsMySpeed
+//
+//  Created by Steve Baker on 11/18/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+// Reference:
+// ExampleAsyncTest
+// http://gabriel.github.com/gh-unit/docs/appledoc_include/guide_testing.html
+
+#import <GHUnitIOS/GHUnit.h>
+
+@interface WeatherAPITests : GHAsyncTestCase { }
+@end
+
+
+@implementation WeatherAPITests
+
+- (void)testURLConnection {
+    
+    // Call prepare to setup the asynchronous action.
+    // This helps in cases where the action is synchronous and the
+    // action occurs before the wait is actually called.
+    [self prepare];
+    
+    NSURLRequest *request = [NSURLRequest
+                             requestWithURL:[NSURL URLWithString:@"http://www.google.com"]];
+    
+    // FIXME: Xcode warns variable connection unused.
+    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request
+                                                                  delegate:self
+                                                          startImmediately:YES];
+    
+    // Wait until notify called for timeout (seconds);
+    // If notify is not called with kGHUnitWaitStatusSuccess then
+    // we will throw an error.
+    [self waitForStatus:kGHUnitWaitStatusSuccess timeout:10.0];
+}
+
+# pragma mark - Event handlers
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    // Notify of success, specifying the method where wait is called.
+    // This prevents stray notifies from affecting other tests.
+    [self notify:kGHUnitWaitStatusSuccess forSelector:@selector(testURLConnection)];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    // Notify of connection failure
+    [self notify:kGHUnitWaitStatusFailure forSelector:@selector(testURLConnection)];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    GHTestLog(@"%@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+}
+
+@end
