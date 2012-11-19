cc41d9d238107e430e27006b093196b5ef3df67f
beepscore <support@beepscore.com>
2012-11-18 17:42:20 -0800

    In Location.m add properties and methods in preparation for testing with OCMock.
    I could see some of this code in video Ch 5 Creating COMock stubs. I inferred the rest, and modified slightly.

--- WhatsMySpeed/Models/Location.m
+++ WhatsMySpeed/Models/Location.m
@@ -8,6 +8,13 @@
 
 #import "Location.h"
 
+@interface Location ()
+@property (nonatomic, strong) CLGeocoder *geocoder;
+@property (nonatomic, assign) BOOL geocodePending;
+@property (nonatomic, strong) NSString *postalCode;
+@end
+
+
 @implementation Location
 
 - (id)init {
@@ -21,17 +28,31 @@
     [_locationManager setDesiredAccuracy:kCLLocationAccuracyBestForNavigation];
     [_locationManager setDistanceFilter:kCLDistanceFilterNone];
     
+    _geocoder = [[CLGeocoder alloc] init];
+    
     return self;
 }
 
 
 - (void)startLocationUpdates {
     [self.locationManager startUpdatingLocation];
-    
 }
 
 
 #pragma mark - Helper methods
+
+- (void)updatePostalCode:(CLLocation *)newLocation
+             withHandler:(CLGeocodeCompletionHandler)completionHandler {
+    
+    if (YES == self.geocodePending) {
+        return;
+    }
+    self.geocodePending = YES;
+    
+    [self.geocoder reverseGeocodeLocation:newLocation
+                        completionHandler:completionHandler];
+}
+
 
 - (float)calculateSpeedInMPH:(CLLocationSpeed)speedInMetersPerSecond {
     NSInteger kSecondsPerHour = 60 * 60;
@@ -48,10 +69,21 @@
     if (!locations
         || (0 == locations.count)) {
         self.speedMilesPerHour = 0.0;
+
+    } else {
+        // we have at least one location
+        // if locations contains multiple objects, the lastObject is most recent.
+        
+        [self updatePostalCode:[locations lastObject]
+                   withHandler:^(NSArray *placemarks, NSError *error) {
+                       CLPlacemark *placemark = placemarks[0];
+                       [self setPostalCode:[placemark postalCode]];
+                       self.geocodePending = NO;
+                   }];
+        
+        // CLLocation speed is meters per second.
+        self.speedMilesPerHour = [self calculateSpeedInMPH:[[locations lastObject] speed]];
     }
-    // if locations contains multiple objects, the lastObject is most recent
-    // CLLocation speed is meters per second.
-    self.speedMilesPerHour = [self calculateSpeedInMPH:[[locations lastObject] speed]];
 }
 
 @end
