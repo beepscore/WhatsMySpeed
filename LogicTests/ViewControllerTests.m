d42161c0387fb5750d15f9e4406dce81b9e5a343
beepscore <support@beepscore.com>
2012-12-09 22:04:45 -0800

    Move code from ViewControllerTests.h to ViewControllerTests.m, delete ViewControllerTests.h. Add ViewController.m to target LogicTests. Implement ViewController.m with testViewControllerNotNil.

--- LogicTests/ViewControllerTests.m
+++ LogicTests/ViewControllerTests.m
@@ -6,8 +6,36 @@
 //  Copyright (c) 2012 Beepscore LLC. All rights reserved.
 //
 
-#import "ViewControllerTests.h"
+#import <SenTestingKit/SenTestingKit.h>
+#import "ViewController.h"
+#import "OCMock.h"
+
+@interface ViewControllerTests : SenTestCase
+@property (nonatomic, strong) ViewController *viewController;
+@end
+
 
 @implementation ViewControllerTests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Set-up code here.
+    self.viewController = [[ViewController alloc] init];
+}
+
+
+- (void)tearDown {
+    // Tear-down code here.
+    self.viewController = nil;
+    
+    [super tearDown];
+}
+
+
+- (void)testViewControllerNotNil {
+    STAssertNotNil(self.viewController,
+                   @"Expected viewController not nil");
+}
 
 @end
