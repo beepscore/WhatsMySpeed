a443e1544f2e1cf623911aad99a89525269dce56
beepscore <support@beepscore.com>
2012-12-09 21:50:27 -0800

    In Xcode add ViewControllerTests.

---
+++ LogicTests/ViewControllerTests.m
@@ -0,0 +1,13 @@
+//
+//  ViewControllerTests.m
+//  WhatsMySpeed
+//
+//  Created by Steve Baker on 12/9/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import "ViewControllerTests.h"
+
+@implementation ViewControllerTests
+
+@end
