e83e322c6c51694f0610fa1c1f45137c8c6c6bd4
beepscore <support@beepscore.com>
2012-12-10 19:18:47 -0800

    In ViewControllerTests add tests. Use a partial mock to test if it calls a method on itself.
    On simulator all 20 logic unit tests pass.

--- LogicTests/ViewControllerTests.m
+++ LogicTests/ViewControllerTests.m
@@ -10,6 +10,7 @@
 #import "ViewController.h"
 #import "ViewController_Extension.h"
 #import "OCMock.h"
+#import "Location.h"
 
 @interface ViewControllerTests : SenTestCase
 @property (nonatomic, strong) ViewController *viewController;
@@ -43,7 +44,6 @@
 - (void)testViewDidLoadSetsUserTrackingMode {
     id mockMapView = [OCMockObject mockForClass:[MKMapView class]];
     
-    //self.viewController.mapView = (MKMapView *)mockMapView;
     self.viewController.mapView = mockMapView;
     
     // Don't throw exception if setUserTrackingMode gets called
@@ -53,6 +53,44 @@
     
     // verify all stubbed or expected methods were called.
     [mockMapView verify];
+}
+
+
+- (void)testViewDidLoadSetsLocation {
+    [self.viewController viewDidLoad];
+    STAssertNotNil(self.viewController.location,
+                   @"Expected location not nil");
+}
+
+
+- (void)testViewDidLoadCallsBeginLocationUpdates {
+    
+    // use a partial mock to test if the viewController calls a method on itself
+    id mockViewController = [OCMockObject
+                             partialMockForObject:self.viewController];
+    
+    // Don't throw exception if beginLocationUpdates gets called.
+    // Xcode complained about argument mockViewController.location, so use any.
+    // [[mockViewController expect] beginLocationUpdates:mockViewController.location];
+    [[mockViewController expect] beginLocationUpdates:[OCMArg any]];
+    
+    [mockViewController viewDidLoad];
+    
+    // use the partial mock to verify all stubbed or expected methods were called.
+    [mockViewController verify];
+}
+
+
+- (void)testBeginLocationUpdatesCallsStartLocationUpdates {
+    id mockLocation = [OCMockObject mockForClass:[Location class]];
+    
+    // Don't throw exception if startLocationUpdates gets called
+    [[mockLocation expect] startLocationUpdates];
+    
+    [self.viewController beginLocationUpdates:mockLocation];
+    
+    // verify all stubbed or expected methods were called.
+    [mockLocation verify];
 }
 
 @end
